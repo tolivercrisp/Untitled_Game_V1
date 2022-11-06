699ab4484b64edf197c2d1ddc49c8b5353407fd5
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-11-06 15:45:33 -0500

    added starter folders like audio, images, tiled, and a gitignore file

---
+++ tiled/tf_ashland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tf_ashland" tilewidth="16" tileheight="16" tilecount="840" columns="40">
+ <image source="../images/tf_ashlands/ashlands_tileset.png" width="640" height="336"/>
+</tileset>
