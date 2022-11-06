4cd4f0f103845798f9d73071f3db28494c120624
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-11-06 18:42:01 -0500

    added new asset pack -- pixel crawler

---
+++ tiled/pixel_crawler.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="pixel_crawler" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="../Pixel_Crawler/Environment/Dungeon Prison/Assets/Props.png" width="400" height="400"/>
+</tileset>
