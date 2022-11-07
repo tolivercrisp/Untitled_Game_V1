27d7146d9c0ae25e72dc758fb21e595af3cedb59
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-11-06 20:05:03 -0500

    added character model + positioned it in the center of the screen

--- index.js
+++ index.js
@@ -10,15 +10,22 @@ const c = canvas.getContext('2d')
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-// variable of underworld_map.png
+// map load
 const image = new Image()
 image.src = './tiled/underworld_map.png'
+
+// player character load
+const playerImage = new Image()
+playerImage.src = './Heavy_Knight/Heavy_Knight_Non-Combat_Animations.png'
 
 // shows the image only once it has loaded
 image.onload = () => {
     // adjusts zoomed image to the center
     c.drawImage(image, -1390, -1025)
+    c.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2, canvas.height / 2 - playerImage.height / 2)
 }
+
+
 
 
 
